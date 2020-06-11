f43b9df396c5d53c7f44fb4a9f5df900ab23b6f5
Vladislav <mandarineye@gmail.com>
2020-06-11 16:42:40 +0300

    Add production environment config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+export const env = {
+  production: false,
+  apiUrlHttp: 'http://remote-control-server-node.herokuapp.com:80',
+  apiUrlWs: 'ws://remote-control-server-node.herokuapp.com:80',
+  // apiUrlHttp: 'http://192.168.0.102:49150',
+  // apiUrlWs: 'ws://192.168.0.102:49150',
 };
